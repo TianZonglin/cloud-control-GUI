a391b0ff28c7c5e16c3d51958dba8c7165509cb1
TianZonglin <1805984583@qq.com>
2020-08-01 13:46:05 +0200

    added comments

--- push.sh
+++ push.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 /usr/bin/git add -A
-/usr/bin/git commit -m "`date +%Y-%m-%d,%H:%m`" 
+#/usr/bin/git commit -m "`date +%Y-%m-%d,%H:%m`" 
+/usr/bin/git commit -m "added comments"
 /usr/bin/git push -u origin master -f
 
 
